d886f4408cae955eedd1de9f6f19014879789e7a
AdrianaGabrielyan <adrin.gabrielyan@gmail.com>
2020-09-22 12:22:50 +0400

    creat project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import HomePage from "./homepage.component";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meningOfLife: 47 + this.props.incriment,
+    };
+  }
+  handleClick = () => {
+    this.setState(
+      (prevSate, prevProps) => {
+        return { meningOfLife: prevSate.meningOfLife + prevProps.incriment };
+      },
+      () => {
+        console.log(this.state.meningOfLife);
+      }
+    );
+  };
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
